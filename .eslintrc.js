cb98eaa6148490a4275f676d1ac7be7f585cddaa
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-05-28 13:02:35 +0200

    Eslint add project resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['jsx-a11y', '@typescript-eslint', 'react-hooks', 'react-refresh'],
+  plugins: ['jsx-a11y', '@typescript-eslint', 'react-hooks', 'react-refresh', 'import'],
   env: {
     browser: true,
     es2021: true,
@@ -22,7 +22,9 @@ module.exports = {
     },
     'import/resolver': {
       node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-      typescript: {},
+      typescript: {
+        project: ['tsconfig.json', 'src/*/tsconfig.json', 'sandbox/tsconfig.json'],
+      },
     },
   },
   rules: {
